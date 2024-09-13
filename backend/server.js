2a8f4570e0408d9aa12c480a3aed9179217754ae
Hirusha Jayarathne <hirushachamod2@gmail.com>
2024-09-13 19:30:38 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import authRoutes from "./routes/authRoute"
+
+
+const express = require('express')
+
+const app = express();
+
+app.use("/api/v1/auth", authRoutes)
+
+app.listen(5000, () => {
+    console.log("Server started at port 5000")
+})
