5c270f8932c8d569b643288ea67430656bcc937c
Hirusha Jayarathne <hirushachamod2@gmail.com>
2024-10-08 20:44:06 +0530

    Refactor environment variables and add JWT authentication

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,6 @@
 import { User } from '../models/user.js'
+import bcryptjs from "bcryptjs"
+import { generateTokenAndSetCookie } from '../utils/generateToke.js';
 
 
 export const signup = async (req, res) => {
@@ -32,18 +34,29 @@ export const signup = async (req, res) => {
             return res.status(400).json({ message: "User with this username already exists" });
         }
 
+        const salt = await bcryptjs.genSalt(10)
+        const hashedPassword = await bcryptjs.hash(password, salt)
+
         const PROFILE_PICS = ["/avatar1.png", "/avatar2.png", "/avatar3.png"]
 
         const image = PROFILE_PICS[Math.floor(Math.random() * PROFILE_PICS.length)];
 
         const newUser = new User({
             email,
-            password,
+            password: hashedPassword,
             username,
             image
         })
 
-        await newUser.save()
+        if (newUser) {
+            generateTokenAndSetCookie(newUser._id, res);
+            await newUser.save()
+            res.status(201).json({ success: true, message: "User created successfully" })
+        } else {
+            res.status(500).json({ success: false, message: "Internal server error" })
+        }
+
+
 
 
     } catch (error) {
@@ -52,9 +65,44 @@ export const signup = async (req, res) => {
 }
 
 export const login = async (req, res) => {
-    res.send("login route ");
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({ success: false, message: "Please fill all the fields" })
+        }
+
+        const user = await User.findOne({ email: email })
+        if (!user) {
+            return res.status(400).json({ success: false, message: "User not found" })
+        }
+
+        const isPasswordCorrect = await bcryptjs.compare(password, user.password);
+
+        if (!isPasswordCorrect) {
+            return res.status(400).json({ success: false, message: "Invalid credentials" })
+        }
+        generateTokenAndSetCookie(user._id, res);
+
+        res.status(200).json({
+            success: true,
+            message: "Logged in successfully",
+            user: {
+                ...user._doc,
+                password: ""
+            }
+        })
+    } catch (error) {
+        console.log("Error in login controller", error.message)
+
+    }
 }
 
 export const logout = async (req, res) => {
-    res.send("logout route ");
+    try {
+        res.clearCookie("jwt-netflix");
+        res.status(200).json({ success: true, message: "Logged out successfully" })
+    } catch (error) {
+        console.log("Error in logout controller", error.message)
+    }
 }
