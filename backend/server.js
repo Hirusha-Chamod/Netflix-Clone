813ab64e2ebe8bae41565db7e0c5d3978de850f7
Hirusha Jayarathne <hirushachamod2@gmail.com>
2024-10-10 00:22:29 +0530

    Refactor project structure and add Tailwind CSS configuration

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,11 @@ import { connectDB } from "./config/db.js";
 import { ENV_VARS } from "./config/envVars.js";
 import authRoutes from "./routes/authRoute.js"
 import express from "express"
+import cookieParser from "cookie-parser"
+import movieRoutes from "./routes/movieRoutes.js"
+import tvRoutes from "./routes/tvRoutes.js"
+import { protectRoute } from "./middlewear/protectRoute.js";
+import searchRoutes from "./routes/searchRoutes.js"
 
 
 
@@ -9,10 +14,15 @@ const app = express();
 const PORT = ENV_VARS.PORT
 
 app.use(express.json())
+app.use(cookieParser())
 
-app.use("/api/v1/auth", authRoutes)
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/movie", protectRoute, movieRoutes);
+app.use("/api/v1/tv", protectRoute, tvRoutes);
+app.use("/api/v1/search", protectRoute, searchRoutes);
 
 app.listen(PORT, () => {
     console.log("Server started at port " + PORT);
     connectDB();
 })
+
---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { Route, Routes } from "react-router-dom"
+import LoginPage from './pages/LoginPage'
+import SignUp from './pages/SignUp'
+import Home from './pages/Home'
+
+function App() {
+
+
+  return (
+    <>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/login' element={<LoginPage/>}></Route>
+      <Route path='/signup' element={<SignUp/>}></Route>
+    </Routes>
+    </>
+  )
+
+}
+
+export default App
